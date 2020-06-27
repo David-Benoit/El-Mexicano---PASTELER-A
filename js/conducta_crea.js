1d20291692cd983545ce332f902ca8cf49632786
Manuel David Sánchez Benoit <65616995+David-Benoit@users.noreply.github.com>
2020-06-27 01:18:29 -0500

    Add files via upload
    se creo la conducta de "catálogo" y de "Crea tu Pastel"
    
    se trabajo en una video llamada en zoom donde todos los integrantes aportamos al código

--- js/conducta_crea.js
+++ js/conducta_crea.js
@@ -1,40 +1,141 @@
-var basep = JSON.parse(base);
 var imgbase = document.getElementById("base")
-var elTotal=document.getElementById("Total")
-var diez =document.getElementById('diezp')
-var treinta =document.getElementById('trep')
-var cincuenta =document.getElementById('cinp')
 var nuperson = 0;
-mostrarProductosb();
-//Funcions mostrar productos
-function mostrarProductosb(){
+var valper =0;
+var relleno = "";
+var pf =0;
+var decorado = "";
+var pd=0;
+
+//variables mostrar productos
   var unNodoTitle = document.createElement('h5');
   unNodoTitle.classList.add('col-sm-12', 'alert', 'alert-custom', 'text-center');
   unNodoTitle.textContent = "Base";
   imgbase.appendChild(unNodoTitle);
+  var unNodo = document.createElement('div');
+  unNodo.classList.add('div','col-sm-12', 'text-center');
 
-  for (var pro of basep){
-    var unNodo = document.createElement('div');
-    unNodo.classList.add('div','col-sm-12', 'text-center');
+  var unNodoCardBody = document.createElement('div');
+  unNodoCardBody.classList.add('div');
 
-    var unNodoCardBody = document.createElement('div');
-    unNodoCardBody.classList.add('div');
+  var unNodoImagen = document.createElement('img');
+  unNodoImagen.classList.add('img-fluid');
+  unNodoImagen.setAttribute('src', 'Images/crea/crea1.png',);
+  unNodoImagen.style.width='400px';
+  unNodoImagen.style.height='400px';
 
-    var unNodoImagen = document.createElement('img');
-    unNodoImagen.classList.add('img-fluid');
-    unNodoImagen.setAttribute('src', pro.imagen,);
-    unNodoImagen.style.width='400px';
-    unNodoImagen.style.height='400px';
+  unNodoCardBody.appendChild(unNodoImagen);
+  unNodo.appendChild(unNodoCardBody);
+  imgbase.appendChild(unNodo);
 
-    unNodoCardBody.appendChild(unNodoImagen);
-    unNodo.appendChild(unNodoCardBody);
-    imgbase.appendChild(unNodo);
+//precios - personas
+function agregar10(){
+  nuperson=10;
+  valper=60;
+}
+function agregar30(){
+  nuperson=30;
+  valper=180;
+}
+function agregar50(){
+  nuperson=50;
+  valper=300;
+}
+
+//rellenos
+function ref(){
+  relleno="Frutas";
+  pf=20;
+unNodoTitle.textContent = "Frutas";
+unNodoImagen.setAttribute('src', 'Images/crea/crea4.png',);
+}
+function rec(){
+  relleno="Chocolate";
+  pf=40;
+unNodoTitle.textContent = "Chocolate";
+unNodoImagen.setAttribute('src', 'Images/crea/crea3.png',);
+}
+function ret(){
+  relleno="Tres Leches";
+  pf=50;
+unNodoTitle.textContent = "Tres Leches";
+unNodoImagen.setAttribute('src', 'Images/crea/crea2.png',);
+}
+
+//Decorados
+function des(){
+  decorado="Sencillo";
+  pd=80;
+  switch (relleno) {
+    case "Frutas":
+      unNodoTitle.textContent = "Frutas - Sencillo";
+      unNodoImagen.setAttribute('src', 'Images/crea/crea4_1.png',);
+      break;
+    case "Chocolate":
+      unNodoTitle.textContent = "Chocolate - Sencillo";
+      unNodoImagen.setAttribute('src', 'Images/crea/crea3_1.png',);
+      break;
+    case "Tres Leches":
+      unNodoTitle.textContent = "Tres Leches - Sencillo";
+      unNodoImagen.setAttribute('src', 'Images/crea/crea2_1.png',);
+      break;
+    default:
 
   }
 }
-//Funcion calcular numero de Personas
-diez.addEventListener("onclick", nuperson=10);
-treinta.addEventListener('click', nuperson=30);
-cincuenta.addEventListener('click', nuperson=50);
+function dem(){
+  decorado="Muy Decorado";
+  pd=160;
+  switch (relleno) {
+    case "Frutas":
+      unNodoTitle.textContent = "Frutas - Muy Decorado";
+      unNodoImagen.setAttribute('src', 'Images/crea/crea4_2.png',);
+      break;
+    case "Chocolate":
+      unNodoTitle.textContent = "Chocolate - Muy Decorado";
+      unNodoImagen.setAttribute('src', 'Images/crea/crea3_2.png',);
+      break;
+    case "Tres Leches":
+      unNodoTitle.textContent = "Tres Leches - Muy Decorado";
+      unNodoImagen.setAttribute('src', 'Images/crea/crea2_2.png',);
+      break;
+    default:
 
-elTotal.addEventListener("click", alert('hola'));
+  }
+}
+function dee(){
+  decorado="Especial";
+  pd=200;
+  switch (relleno) {
+    case "Frutas":
+      unNodoTitle.textContent = "Frutas - Especial";
+      unNodoImagen.setAttribute('src', 'Images/crea/crea4_3.png',);
+      break;
+    case "Chocolate":
+      unNodoTitle.textContent = "Chocolate - Especial";
+      unNodoImagen.setAttribute('src', 'Images/crea/crea3_3.png',);
+      break;
+    case "Tres Leches":
+      unNodoTitle.textContent = "Tres Leches - Especial";
+      unNodoImagen.setAttribute('src', 'Images/crea/crea2_3.png',);
+      break;
+    default:
+
+  }
+}
+
+
+//total - Limpiar
+function calcularTotal(){
+  var netv = valper+pf+pd;
+  }
+function lim(){
+  alert('hula');
+  nuperson = 0;
+  valper =0;
+  relleno = "";
+  pf =0;
+  decorado = "";
+  pd=0;
+  unNodoTitle.textContent = "Base";
+  unNodoImagen.setAttribute('src', 'Images/crea/crea1.png');
+}
